34566b074066153e38d668e21b79741a350374dd
Richard McQueary <56047849+richardMcQueary@users.noreply.github.com>
2021-09-27 15:24:04 -0400

    Update ConfigurationXmlPersisterProvider.java

--- app/src/main/java/org/jd/gui/service/configuration/ConfigurationXmlPersisterProvider.java
+++ app/src/main/java/org/jd/gui/service/configuration/ConfigurationXmlPersisterProvider.java
@@ -95,6 +95,25 @@ public class ConfigurationXmlPersisterProvider implements ConfigurationPersister
         UIManager.put("Button.font", new FontUIResource(new Font("Dialog", Font.BOLD, 18)));
         UIManager.put("TextField.font", new FontUIResource(new Font("Dialog", Font.PLAIN, 18)));
         
+        /*
+         *  DARK MODE
+         */
+        UIManager.put( "control", new Color( 128, 128, 128) );
+        UIManager.put( "info", new Color(128,128,128) );
+        UIManager.put( "nimbusBase", new Color( 18, 30, 49) );
+        UIManager.put( "nimbusAlertYellow", new Color( 248, 187, 0) );
+        UIManager.put( "nimbusDisabledText", new Color( 128, 128, 128) );
+        UIManager.put( "nimbusFocus", new Color(115,164,209) );
+        UIManager.put( "nimbusGreen", new Color(176,179,50) );
+        UIManager.put( "nimbusInfoBlue", new Color( 66, 139, 221) );
+        UIManager.put( "nimbusLightBackground", new Color( 18, 30, 49) );
+        UIManager.put( "nimbusOrange", new Color(191,98,4) );
+        UIManager.put( "nimbusRed", new Color(169,46,34) );
+        UIManager.put( "nimbusSelectedText", new Color( 255, 255, 255) );
+        UIManager.put( "nimbusSelectionBackground", new Color( 104, 93, 156) );
+        UIManager.put( "text", new Color( 230, 230, 230) );
+        
+        
         File recentSaveDirectory = new File(System.getProperty("user.dir"));
 
         config.setRecentLoadDirectory(recentSaveDirectory);
